8a047d9cdbac884a911b9bb051ac7c81e7bd15d5
Ayush <lit2019035@iiitl.ac.in>
2022-09-25 14:56:08 +0530

    updated Admin and Student Home

--- app/src/main/java/com/example/iiitl_elective_selector_app/AdminPortal/AdminPortal.java
+++ app/src/main/java/com/example/iiitl_elective_selector_app/AdminPortal/AdminPortal.java
@@ -2,6 +2,7 @@ package com.example.iiitl_elective_selector_app.AdminPortal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,81 +19,16 @@ import com.google.firebase.auth.FirebaseAuth;
 public class AdminPortal extends AppCompatActivity {
 
     ImageView logout;
-    RelativeLayout sem1, sem2, sem3, sem4, sem5, sem6, sem7, sem8;
+
     FirebaseAuth mAuth;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_portal);
-
-        logout = findViewById(R.id.logout);
+        logout = findViewById(R.id.logoutIV);
         mAuth = FirebaseAuth.getInstance();
-
-        sem1 = findViewById(R.id.sem1);
-        sem2 = findViewById(R.id.sem2);
-        sem3 = findViewById(R.id.sem3);
-        sem4 = findViewById(R.id.sem4);
-        sem5 = findViewById(R.id.sem5);
-        sem6 = findViewById(R.id.sem6);
-        sem7 = findViewById(R.id.sem7);
-        sem8 = findViewById(R.id.sem8);
-
-        sem1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem5.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem6.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        sem8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
